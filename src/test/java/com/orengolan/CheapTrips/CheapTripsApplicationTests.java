673501cc06773d09b895d5417e039b40d53268a1
oren-golan <111655672+oren-golan@users.noreply.github.com>
2023-09-23 20:54:51 +0300

    Initial commit

---
+++ src/test/java/com/orengolan/CheapTrips/CheapTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orengolan.CheapTrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapTripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
